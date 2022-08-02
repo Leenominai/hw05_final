3b78b5be08d67b4554df34356fe18be19dbdd8d6
Александр Рассыхаев <farexzon@yandex.ru>
2022-08-02 21:37:06 +0300

    Sprint #6 Project v2.24

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -47,9 +47,9 @@ def profile(request, username):
     author = get_object_or_404(User, username=username)
     page_obj = get_paginator(author.posts.all(), request)
     following = (
-            request.user.is_authenticated and Follow.objects.filter(
-                user=request.user, author=author)
-            .exists()
+        request.user.is_authenticated and Follow.objects.filter(
+            user=request.user, author=author
+        ).exists()
     )
     context = {
         'author': author,
